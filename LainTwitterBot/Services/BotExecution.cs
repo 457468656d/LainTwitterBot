970b3424b7b16bb555c4be942d050b4f2abf6277
457468656d <ethemsahbaz@outlook.com>
2023-04-10 23:32:49 +0200

    Added BotExection Class
    Added SendImageTweet Method

---
+++ LainTwitterBot/Services/BotExecution.cs
@@ -0,0 +1,38 @@
+﻿using LainTwitterBot.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tweetinvi;
+using Tweetinvi.Models;
+using Tweetinvi.Parameters;
+
+namespace LainTwitterBot.Services
+{
+    public class BotExecution : IBotExecution
+    {
+        private readonly TwitterClient _twitterClient;
+        private readonly IImagesProvider _imagesProvider;
+
+        public BotExecution(TwitterClient twitterClient, IImagesProvider imagesProvider)
+        {
+            _twitterClient = twitterClient;
+            _imagesProvider = imagesProvider;
+        }
+
+        public async Task SendImageTweet()
+        {
+            var imageBytes= _imagesProvider.GetRandomImageAsByteArray();
+            var uploadedMedia = await _twitterClient.Upload.UploadBinaryAsync(imageBytes);
+
+            var tweetParams = new PublishTweetParameters
+            {
+                Medias = new List<IMedia> { uploadedMedia }
+            };
+
+            var tweet = await _twitterClient.Tweets.PublishTweetAsync(tweetParams);
+
+        }
+    }
+}
