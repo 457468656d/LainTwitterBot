669af3ce6b4cec72f6b3f0f099ac1d211dcee313
457468656d <ethemsahbaz@outlook.com>
2023-04-10 23:34:02 +0200

    Added Quartz.NET Scheduler

--- LainTwitterBot/Program.cs
+++ LainTwitterBot/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace LainTwitterBot
+﻿using LainTwitterBot.QuartzJobs;
+using LainTwitterBot.Services;
+using Quartz.Impl;
+using Quartz;
+
+namespace LainTwitterBot
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            StdSchedulerFactory factory = new StdSchedulerFactory();
+            IScheduler scheduler = await factory.GetScheduler();
+
+            await scheduler.Start();
+
+            IJobDetail job = JobBuilder.Create<BotExecutionJob>()
+                .WithIdentity("BotExecution", "Bot")
+                .Build();
+
+
+            ITrigger trigger = TriggerBuilder.Create()
+                .WithIdentity("DailyTrigger", "Bot")
+                .StartNow()
+                .WithSimpleSchedule(x => x
+                    //.WithIntervalInSeconds(10)
+                    .WithIntervalInHours(1)
+                    .RepeatForever())
+                .Build();
+
+            await scheduler.ScheduleJob(job, trigger);
+           
+            Console.ReadKey();
+
+            await scheduler.Shutdown();
         }
     }
 }
