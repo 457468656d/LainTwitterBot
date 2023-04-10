e3bb2770fbc53339a7d0fe4399a6e77ed980a668
457468656d <ethemsahbaz@outlook.com>
2023-04-10 23:33:25 +0200

    Added ImageProvider Class
    Added method to pick a random image.

---
+++ LainTwitterBot/Services/ImagesProvider.cs
@@ -0,0 +1,35 @@
+﻿using LainTwitterBot.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LainTwitterBot.Services
+{
+    public class ImagesProvider : IImagesProvider
+    {
+        private readonly string _imageDirectory;
+
+        public ImagesProvider(string imageDirectory)
+        {
+            _imageDirectory = imageDirectory;
+        }
+
+
+
+        public byte[] GetRandomImageAsByteArray()
+        {
+            string[] files = Directory.GetFiles(_imageDirectory, "*.png");
+
+            int maxRange= files.Length - 1;
+
+            Random r = new Random();
+            int rInt = r.Next(0, maxRange);
+
+            byte[] imageByteArray = File.ReadAllBytes(files[rInt]);
+
+            return imageByteArray;
+        }
+    }
+}
