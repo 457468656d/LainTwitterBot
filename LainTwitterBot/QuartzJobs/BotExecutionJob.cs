fdc6ba4b2aaf217c2b58080384a2865d48afb68a
457468656d <ethemsahbaz@outlook.com>
2023-04-12 16:33:14 +0200

    Added Logging

--- LainTwitterBot/QuartzJobs/BotExecutionJob.cs
+++ LainTwitterBot/QuartzJobs/BotExecutionJob.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Quartz;
+using Serilog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,27 +11,38 @@ using System.Threading.Tasks;
 using LainTwitterBot.Services;
 using Microsoft.Extensions.Configuration;
 using Tweetinvi;
+using Microsoft.Extensions.Logging;
 
 namespace LainTwitterBot.QuartzJobs
 {
+    /// <summary>
+    /// Class for setting up Botlogic as a Quartz.NET job.
+    /// </summary>
     public class BotExecutionJob : IJob
     {
         private readonly IHost _host;
         public BotExecutionJob()
         {
             _host = Host.CreateDefaultBuilder()
+                .UseSerilog((host, loggerConfiguration) =>
+                {
+                    loggerConfiguration.WriteTo.File("Logs.txt", rollingInterval: RollingInterval.Month);
+                    loggerConfiguration.WriteTo.Console();
+                })
                 .ConfigureServices((hostContext, services) =>
                 {
-                    string consumerKey = hostContext.Configuration.GetValue<string>("ConsumerKey");
-                    string consumerSecret = hostContext.Configuration.GetValue<string>("ConsumerSecret");
-                    string accessToken = hostContext.Configuration.GetValue<string>("AccessToken");
-                    string accessTokenSecret = hostContext.Configuration.GetValue<string>("AccessTokenSecret");
+                    string? consumerKey = hostContext.Configuration.GetValue<string>("ConsumerKey");
+                    string? consumerSecret = hostContext.Configuration.GetValue<string>("ConsumerSecret");
+                    string? accessToken = hostContext.Configuration.GetValue<string>("AccessToken");
+                    string? accessTokenSecret = hostContext.Configuration.GetValue<string>("AccessTokenSecret");
 
                     services.AddSingleton((s) => new TwitterClient(consumerKey, consumerSecret, accessToken, accessTokenSecret));
 
                     services.AddSingleton<IBotExecution, BotExecution>();
-                    services.AddSingleton<IImagesProvider>(
-                        new ImagesProvider(hostContext.Configuration.GetValue<string>("ImagesPath")));
+                    services.AddSingleton<IImagesProvider>((s)=>
+                        new ImagesProvider(
+                            hostContext.Configuration.GetValue<string>("ImagesPath"),
+                            s.GetRequiredService<ILogger<IImagesProvider>>()));
 
                 })
                 .Build();
@@ -38,11 +50,19 @@ namespace LainTwitterBot.QuartzJobs
         public async Task Execute(IJobExecutionContext context)
         {
             await _host.StartAsync();
-
-            var botExecution = _host.Services.GetRequiredService<IBotExecution>();
-            await botExecution.SendImageTweet();
-
-            _host.Dispose();
+            try
+            {
+                var botExecution = _host.Services.GetRequiredService<IBotExecution>();
+                await botExecution.SendImageTweet();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                _host.Dispose();
+            }
         }
     }
 }
--- LainTwitterBot/Services/BotExecution.cs
+++ LainTwitterBot/Services/BotExecution.cs
@@ -1,4 +1,5 @@
 ﻿using LainTwitterBot.Interfaces;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,25 +15,45 @@ namespace LainTwitterBot.Services
     {
         private readonly TwitterClient _twitterClient;
         private readonly IImagesProvider _imagesProvider;
+        private readonly ILogger<BotExecution> _logger;
 
-        public BotExecution(TwitterClient twitterClient, IImagesProvider imagesProvider)
+        public BotExecution(TwitterClient twitterClient,
+            IImagesProvider imagesProvider,
+            ILogger<BotExecution> logger)
         {
             _twitterClient = twitterClient;
             _imagesProvider = imagesProvider;
+            _logger = logger;
         }
 
         public async Task SendImageTweet()
         {
-            var imageBytes= _imagesProvider.GetRandomImageAsByteArray();
-            var uploadedMedia = await _twitterClient.Upload.UploadBinaryAsync(imageBytes);
-
-            var tweetParams = new PublishTweetParameters
+            try
             {
-                Medias = new List<IMedia> { uploadedMedia }
-            };
+                var imageBytes = _imagesProvider.GetNextImageAsByteArray();
 
-            var tweet = await _twitterClient.Tweets.PublishTweetAsync(tweetParams);
+                _logger.Log(LogLevel.Information, "Uploading Image to Twittermedia...");
 
+                var uploadedMedia = await _twitterClient.Upload.UploadBinaryAsync(imageBytes);
+                _logger.Log(LogLevel.Information, "Successfully uploaded.");
+
+                var tweetParams = new PublishTweetParameters
+                {
+                    Text = "#SerialExperimentsLain",
+                    Medias = new List<IMedia> { uploadedMedia }
+                };
+
+                _logger.Log(LogLevel.Information, "Publishing tweet...");
+                var tweet = await _twitterClient.Tweets.PublishTweetAsync(tweetParams);
+
+                _logger.Log(LogLevel.Information, "Successfully published tweet.");
+            }
+            catch (Exception ex)
+            {
+
+                _logger.Log(LogLevel.Error, "Could not send tweet. Error:\n" +
+                    ex.Message);
+            }
         }
     }
 }
